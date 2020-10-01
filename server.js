c23ace53cd51959aee49433110b4f145ba472e57
kreid333 <kai.j.reid@gmail.com>
2020-10-01 17:43:36 -0400

    added API route

--- server.js
+++ server.js
@@ -9,16 +9,21 @@ const PORT = process.env.PORT || 8080;
 // Defining app
 const app = express();
 
+// API routes
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
 // HTML routes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Starting server
 app.listen(PORT, () => {
-    console.log(`Your server is on at PORT ${PORT}...`);
-})
+  console.log(`Your server is on at PORT ${PORT}...`);
+});
