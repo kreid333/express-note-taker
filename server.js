6d43efedcf6f93c19879553392d469f18503fa4f
kreid333 <kai.j.reid@gmail.com>
2020-09-30 22:43:38 -0400

    added HTML routes and app.listen

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ const PORT = process.env.PORT || 8080;
 
 // Defining app
 const app = express();
+
+// HTML routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Starting server
+app.listen(PORT, () => {
+    console.log(`Your server is on at PORT ${PORT}...`);
+})
