7346b5547e7bb6a596aed5c7da777bea33c5cb13
kreid333 <kai.j.reid@gmail.com>
2020-09-30 22:42:30 -0400

    defining needed variables

---
+++ server.js
@@ -0,0 +1,10 @@
+// Defining needed modules
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// Defining PORT
+const PORT = process.env.PORT || 8080;
+
+// Defining app
+const app = express();
