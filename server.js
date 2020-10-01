534fab2929fc6b3d5212f1d05f9c2155b173b5aa
kreid333 <kai.j.reid@gmail.com>
2020-10-01 18:27:42 -0400

    added middleware and route for POST method

--- server.js
+++ server.js
@@ -9,9 +9,25 @@ const PORT = process.env.PORT || 8080;
 // Defining app
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // API routes
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const receivedData = JSON.parse(data);
+    if(err) throw err;
+    receivedData.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(receivedData), (err) => {
+      if (err) throw err;
+    });
+    res.send("Done!"); 
+  });
 });
 
 // HTML routes
