7e5283a56c26992fcaf6fe99e6bc3e2b278b86e0
kreid333 <kai.j.reid@gmail.com>
2020-10-02 15:38:30 -0400

    added route for DELETE method along with req.body.id in POST method

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
 app.get("/api/notes", (req, res) => {
@@ -22,12 +23,30 @@ app.post("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     const receivedData = JSON.parse(data);
     if(err) throw err;
+    req.body.id = receivedData.length + 1;
     receivedData.push(req.body);
     fs.writeFile("./db/db.json", JSON.stringify(receivedData), (err) => {
       if (err) throw err;
     });
     res.send("Done!"); 
   });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const paramId = parseInt(req.params.id);
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    const dbArray = JSON.parse(data);
+    for (let i = 0; i < dbArray.length; i++) {
+      if (paramId === dbArray[i].id) {
+        dbArray.splice(dbArray[i], 1);
+        fs.writeFile("./db/db.json", JSON.stringify(dbArray, null, 2), (err) => {
+          if (err) throw err;
+        });
+      }
+    };
+  });
+  res.end("Sucess!");
 });
 
 // HTML routes
